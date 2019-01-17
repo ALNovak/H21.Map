b80e0a83f36b44aac318fbb1beb278c1c74f3f2f
Андрей Смирнов <asmirnov@dc300.local>
2019-01-17 20:00:45 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14882,7 +14882,7 @@ export class AppComponent {
     zoom: 3,
     minZoom: 4,
     maxZoom: 22,
-    provider: MapType.LEAFLET,
+    provider: MapType.BAIDU,
     enableDefaultControl: false,
     enableClick: false,
   }
